73dd89e1d57075612b8c5846acfa08e58667c21b
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2021-11-01 22:48:52 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,8 @@
 sha256 = require 'sha256'
 peripheral.find('modem',rednet.open)
-payload = 'print("hi")'
+print('password:')
+passwd = read("*")
+code = fs.open('code.lua','r')
+payload = code.readAll()
+code.close()
 rednet.broadcast(sha256(passwd..payload..string.format('%d',math.floor(os.epoch()/1000)+os.computerID()))..payload,'control')
